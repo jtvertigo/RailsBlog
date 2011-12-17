1f00baad447ffa742674b231e2c12960a25a1679
Vertigo2 <vertigo2@vertigo2-VirtualBox.(none)>
2011-12-17 13:28:53 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = 'eeac31b6a39b0be459211b1d1610d5b2089998d4b81b42ed51be8220bc9ab379bc64f52a6e02321460f141348eec4b8084baf6a0d0ef5a55578fcc0e5f665284'
